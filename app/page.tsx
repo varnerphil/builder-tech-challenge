4c6e9cadfc69d439c70b8a0753d43f2e7d24d589
Phil Varner <philvarnerlp9@gmail.com>
2025-05-21 09:15:19 -0600

    test homepage

--- app/page.tsx
+++ app/page.tsx
@@ -11,33 +11,19 @@ interface PageProps {
   };
 }
 
-export default async function Homepage(props: PageProps) {
-  const builderModelName = "page";
-  
-  // Get locale from cookies or default to "en-US"
-  const cookieStore = cookies();
-  const locale = cookieStore.get("locale")?.value || "en-US";
-  console.log("locale", locale);
-
-  const content = await builder
-    // Get the page content from Builder with the specified options
-    .get(builderModelName, {
-      userAttributes: {
-        // Use the page path specified in the URL to fetch the content
-        urlPath: "/",
-        options: {
-          locale: locale,
-          enrich: false
-        }
-      },
+export default async function Page(props: PageProps) {
+  const homepageContent = await builder
+    .get('homepage', {
+      prerender: false,
     })
-    // Convert the result to a promise
     .toPromise();
 
   return (
     <>
       {/* Render the Builder page */}
-      <RenderBuilderContent content={content} model={builderModelName} options={{ locale: locale, enrich: false }} />
+      <RenderBuilderContent 
+        model="homepage" 
+        content={homepageContent} />
     </>
   );
 }
